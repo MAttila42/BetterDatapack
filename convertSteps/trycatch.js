d6257d919ecb285f0e1c853eaff04f298c847a9e
ExAtom <menyhart.atika@gmail.com>
2022-03-10 10:31:47 +0100

    1.5.0-dev-7 - TryCatch not-so-progress

--- convertSteps/trycatch.js
+++ convertSteps/trycatch.js
@@ -15,6 +15,8 @@ function trycatch(f, filePath, bdconfig) {
 		let start = tryRegex.exec(f);
 		while (start != null) { // Look through the matches
 			let full = start[0], // Will contain the full result
+				tryBlock = "", // Will contain the content of the try block
+				catchBlock = "", // Will contain the content of the catch block
 				open = 1,
 				close = 0;
 			for (let i of f.slice(tryRegex.lastIndex)) { // Go throught the string one character at a time
@@ -23,7 +25,7 @@ function trycatch(f, filePath, bdconfig) {
 				if (i == '}') close++;
 				if (open == close) break; // If every braces are closed then we have the full result
 			}
-			if (full == lastContent.trim()) continue; // If this result is the content of the last result then don't do anything with it
+			if (lastContent.includes(full)) continue; // If this result is the content of the last result then don't do anything with it
 
 			let tryContent = /(?<=\{).*(?=\})/gms;
 		}
